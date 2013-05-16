b383c6b66f7423d928b71721d88201f61106b217
Eldorados <eldorado.xbmc@gmail.com>
2013-05-16 12:00:10 -0400

    Indendation fix Added Else before raising exception

--- lib/urlresolver/plugins/stagevu.py
+++ lib/urlresolver/plugins/stagevu.py
@@ -47,13 +47,14 @@ class StagevuResolver(Plugin, UrlResolver, PluginSettings):
             match=p.findall(link)
             if match:
                 return match[0]
-            raise Exception ('File Not Found or removed')
+            else:
+                raise Exception ('File Not Found or removed')
         except urllib2.URLError, e:
             common.addon.log_error(self.name + ': got http error %d fetching %s' %
                                    (e.code, web_url))
             common.addon.show_small_popup('Error','Http error: '+str(e), 8000, error_logo)
             return False
-       except Exception, e:
+        except Exception, e:
             common.addon.log('**** Stagevu Error occured: %s' % e)
             common.addon.show_small_popup(title='[B][COLOR white]STAGEVU[/COLOR][/B]', msg='[COLOR red]%s[/COLOR]' % e, delay=5000, image=error_logo)
             return False 
