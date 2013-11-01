1c7d52f6d471b2aa79975abe0e4373fe9b8ce041
worstperson <xmoser@gmail.com>
2013-10-31 23:57:59 -0500

    Made requested changes

--- lib/urlresolver/plugins/promptfile.py
+++ lib/urlresolver/plugins/promptfile.py
@@ -55,17 +55,18 @@ class PromptfileResolver(Plugin, UrlResolver, PluginSettings):
             common.addon.log_error(self.name + ': got http error %d fetching %s' %
                                    (e.code, web_url))
             common.addon.show_small_popup('Error','Http error: '+str(e), 5000, error_logo)
-            return False
+            return self.unresolvable(code=3, msg=e)
+
         except Exception, e:
             common.addon.log_error('**** Promptfile Error occured: %s' % e)
             common.addon.show_small_popup(title='[B][COLOR white]PROMPTFILE[/COLOR][/B]', msg='[COLOR red]%s[/COLOR]' % e, delay=5000, image=error_logo)
-            return False
+            return self.unresolvable(code=0, msg=e)
 
     def get_url(self, host, media_id):
-        return 'http://%s/l/%s' % (host, media_id)
+        return 'http://www.promptfile.com/%s' % (media_id)
 
     def get_host_and_id(self, url):
-        r = re.search('//(.+?)/l/([0-9a-zA-Z\-]+)', url)
+        r = re.search('//(.+?)/(.+)', url)
         if r:
             return r.groups()
         else:
