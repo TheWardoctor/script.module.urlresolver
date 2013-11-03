949dedaabfcd6c83ad1b90466a9131e0e2e408f0
Eldorados <eldorado.xbmc@gmail.com>
2013-11-03 17:54:15 -0500

    Update lemuploads.py
    Removing copyright notice - urlresolver open source, no site wars stuff needed, and this was Icefilms code :)

--- lib/urlresolver/plugins/lemuploads.py
+++ lib/urlresolver/plugins/lemuploads.py
@@ -1,6 +1,5 @@
 '''
 Lemuploads urlresolver plugin
-Copyright (C) 2013 Xunity Talk
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
