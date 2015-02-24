8f01110ccfe1574249b609019d4bdaa1a26de4d4
y2000j <y2000j@yandex.ru>
2015-02-24 11:53:10 +0100

    Update streamcloud.py
    Tab Problem Fix at Line 37 domains

--- lib/urlresolver/plugins/streamcloud.py
+++ lib/urlresolver/plugins/streamcloud.py
@@ -1,6 +1,6 @@
 """
 streamcloud urlresolver plugin
-Copyright (C) 2012 Lynx187
+Copyright (C) 2012 Lynx187 
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
