d7f78eb750a3628ee63eded08f5e88ef600a6362
Tracy Norris <tknorris@gmail.com>
2015-02-28 00:00:56 -0500

    fix cloudyvideos resolver

--- lib/urlresolver/plugins/cloudyvideos.py
+++ lib/urlresolver/plugins/cloudyvideos.py
@@ -31,7 +31,7 @@ error_logo = os.path.join(common.addon_path, 'resources', 'images', 'redx.png')
 class CloudyvideosResolver(Plugin, UrlResolver, PluginSettings):
     implements = [UrlResolver, PluginSettings]
     name = "cloudyvideos"
-    domains = [ "cloudyvideos.com" ]
+    domains = ["cloudyvideos.com"]
 
     def __init__(self):
         p = self.get_setting('priority') or 100
@@ -41,20 +41,26 @@ class CloudyvideosResolver(Plugin, UrlResolver, PluginSettings):
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         try:
-            html = self.net.http_GET(web_url).content           
+            html = self.net.http_GET(web_url).content
             form_values = {}
             for i in re.finditer('<input type="hidden" name="(.*?)" value="(.*?)"', html):
-                form_values[i.group(1)] = i.group(2)   
+                form_values[i.group(1)] = i.group(2)
             html = self.net.http_POST(web_url, form_data=form_values).content
-            r = re.search("file: '([^']+)'",html)
-            if not r:
-                raise Exception ('Unable to resolve cloudyvideos link. Filelink not found.')
-            return r.group(1)
-        
+            for match in re.finditer('(eval\(function.*?)</script>', html, re.DOTALL):
+                js_data = jsunpack.unpack(match.group(1))
+                match2 = re.search('<param\s+name="src"\s*value="([^"]+)', js_data)
+                if match2:
+                    return match2.group(1)
+                else:
+                    match2 = re.search('<embed.*?type="video.*?src="([^"]+)', js_data)
+                    if match2:
+                        return match2.group(1)
+                
+            raise Exception('Unable to resolve cloudyvideos link. Filelink not found.')
         except urllib2.URLError, e:
             common.addon.log_error(self.name + ': got http error %d fetching %s' %
                                     (e.code, web_url))
-            return self.unresolvable(code=3, msg='Exception: %s' % e) 
+            return self.unresolvable(code=3, msg='Exception: %s' % e)
         except Exception, e:
             common.addon.log('**** cloudyvideos Error occured: %s' % e)
             return self.unresolvable(code=0, msg='Exception: %s' % e)
@@ -73,8 +79,5 @@ class CloudyvideosResolver(Plugin, UrlResolver, PluginSettings):
             else:
                 return False
 
-
     def valid_url(self, url, host):
         return re.match('http://(www.)?cloudyvideos.com/[0-9A-Za-z]+', url) or 'cloudyvideos' in host
-
-
