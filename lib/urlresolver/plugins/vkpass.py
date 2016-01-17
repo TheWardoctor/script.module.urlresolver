5f450a43af5ee1f33b492acc94b8edd1554f250c
tknorris <tknorris@tknorris-laptop>
2016-01-17 17:48:26 -0500

    add seberoth's vkpass fix

--- lib/urlresolver/plugins/vkpass.py
+++ lib/urlresolver/plugins/vkpass.py
@@ -39,45 +39,76 @@ class VKPassResolver(Plugin, UrlResolver, PluginSettings):
         soup = self.net.http_GET(base_url).content
         html = soup.decode('cp1251')
         vBlocks = re.findall('{(file.*?label.*?)}', html)
+        html5 = re.findall('}\((.*?)\)\)<', html)
 
-        if vBlocks:
-            purged_jsonvars = {}
-            lines = []
-            best = '0'
-
-            for block in vBlocks:
-                vItems = re.findall('([a-z]*):"(.*?)"', block)
-                if vItems:
-                    quality = ''
-                    url = ''
-
-                    for item in vItems:
-                        if 'file' in item[0]:
-                            url = item[1]
-                        if 'label' in item[0]:
-                            quality = re.sub("[^0-9]", "", item[1])
-                            lines.append(quality)
-                            if int(quality) > int(best): best = quality
-
-                    purged_jsonvars[quality] = url
-                else:
-                    raise UrlResolver.ResolverError('No file found')
-
-            lines = sorted(lines, key=int)
-
-            if len(lines) == 1:
-                return purged_jsonvars[lines[0]].encode('utf-8')
-            else:
-                if self.get_setting('auto_pick') == 'true':
-                    return purged_jsonvars[str(best)].encode('utf-8') + '|User-Agent=%s' % (common.IE_USER_AGENT)
-                else:
-                    result = xbmcgui.Dialog().select('Choose the link', lines)
-            if result != -1:
-                return purged_jsonvars[lines[result]].encode('utf-8') + '|User-Agent=%s' % (common.IE_USER_AGENT)
-            else:
-                raise UrlResolver.ResolverError('No link selected')
-        else:
+        if not vBlocks and not html5:
             raise UrlResolver.ResolverError('No vsource found')
+
+        data = dict()
+        data['purged_jsonvars'] = {}
+        data['lines'] = []
+        data['best'] = '0'
+
+        if html5:
+            for source in html5:
+                params = source.split(',')
+                data = self.__decodeLinks(params[0], params[3].split('|'), data)
+        elif vBlocks:
+            data = self.__getFlashVids()
+
+        data['lines'] = sorted(data['lines'], key=int)
+
+        if len(data['lines']) == 1:
+            return data['purged_jsonvars'][data['lines'][0]].encode('utf-8')
+        else:
+            if self.get_setting('auto_pick') == 'true':
+                return data['purged_jsonvars'][str(data['best'])].encode('utf-8') + '|User-Agent=%s' % (common.IE_USER_AGENT)
+            else:
+                result = xbmcgui.Dialog().select('Choose the link', data['lines'])
+        if result != -1:
+            return data['purged_jsonvars'][data['lines'][result]].encode('utf-8') + '|User-Agent=%s' % (common.IE_USER_AGENT)
+        else:
+            raise UrlResolver.ResolverError('No link selected')
+
+    def __decodeLinks(self, html, list, data):
+        if "source" not in list:
+            return data
+
+        numerals="0123456789abcdefghijklmnopqrstuvwxyz"
+        letters = re.findall('([0-9a-z])', html)
+        for letter in letters:
+            html = re.sub('\\b' + letter + '\\b', list[numerals.index(letter)], html)
+
+        sources = re.findall('<source.*?>', html)
+
+        for source in sources:
+            url = re.findall('src="(.*?)"', source)
+            res = re.findall('res="(.*?)"', source)
+
+            data['lines'].append(res[0])
+            data['purged_jsonvars'][res[0]] = url[0]
+            if int(res[0]) > int(data['best']): data['best'] = res[0]
+
+        return data
+
+    def __getFlashVids(self, vBlocks, data):
+        for block in vBlocks:
+            vItems = re.findall('([a-z]*):"(.*?)"', block)
+            if vItems:
+                quality = ''
+                url = ''
+
+                for item in vItems:
+                    if 'file' in item[0]:
+                        url = item[1]
+                    if 'label' in item[0]:
+                        quality = re.sub("[^0-9]", "", item[1])
+                        data['lines'].append(quality)
+                        if int(quality) > int(data['best']): data['best'] = quality
+
+                data['purged_jsonvars'][quality] = url
+
+        return data
 
     def get_url(self, host, media_id):
         return 'http://%s/token/%s' % (host, media_id)
@@ -90,6 +121,8 @@ class VKPassResolver(Plugin, UrlResolver, PluginSettings):
             return False
 
     def valid_url(self, url, host):
+        if self.get_setting('enabled') == 'false':
+            return False
         return re.search(self.pattern, url) or 'vkpass' in host
 
     def get_settings_xml(self):
